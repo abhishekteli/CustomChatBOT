901a86b7474d9538c38a1787dc9101b5b1348439
Abhishek Teli <ateli333@gmail.com>
2024-01-09 10:51:19 -0500

    Update main.py
    Updated the code to PDF as input

--- main.py
+++ main.py
@@ -1,26 +1,39 @@
-from langchain. document_loaders import TextLoader
+from langchain. document_loaders import TextLoader, PyPDFLoader
 from langchain.text_splitter import CharacterTextSplitter
 from langchain.vectorstores.chroma import Chroma
 from langchain.embeddings import OpenAIEmbeddings
 from dotenv import load_dotenv
 
-load_dotenv()
+def initization():
+    load_dotenv()
+    embeddings = OpenAIEmbeddings()
+    text_splitter = CharacterTextSplitter(
+        separator="\n",
+        chunk_size =200,
+        chunk_overlap=0
+        )
 
-embeddings = OpenAIEmbeddings()
+    return text_splitter, embeddings
 
-text_splitter = CharacterTextSplitter(
-    separator="\n",
-    chunk_size =200,
-    chunk_overlap=0
-)
+def file_load(file_type,file_location, text_splitter, embeddings):
 
-loader = TextLoader("facts.txt")
-docs = loader.load_and_split(
-    text_splitter=text_splitter
-)
+    if file_type == "PDF":
+        loader = PyPDFLoader(file_path=file_location)
+    else:
+        loader = TextLoader(file_path=file_location)
 
-db = Chroma.from_documents(
-    docs,
-    embedding=embeddings,
-    persist_directory="emb"
-)
+    docs = loader.load_and_split(
+        text_splitter=text_splitter
+    )
+
+    db = Chroma.from_documents(
+        docs,
+        embedding=embeddings,
+        persist_directory="emb"
+    )
+
+if __name__=="__main__":
+    text_splitter, embeddings = initization()
+    file_type = input("Enter the File type: ")
+    file_location = input("Enter the File location: ")
+    file_load(file_type, file_location, text_splitter, embeddings)
